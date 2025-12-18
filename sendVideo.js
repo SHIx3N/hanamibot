83ba44561abd9851e745f782f0f37052e04f8d0f
SHIx3N <shin.7cchi@gmail.com>
2025-12-18 21:08:13 +0900

    Update sendVideo.js

--- sendVideo.js
+++ sendVideo.js
@@ -11,9 +11,9 @@ async function main() {
   await client.pushMessage(TO, {
     type: 'video',
     originalContentUrl:
-      'https://github.com/SHIx3N/hanamibot/sleep.mp4',
+      'https://github.com/SHIx3N/hanamibot/blob/main/docs/sleep.mp4',
     previewImageUrl:
-      'https://github.com/SHIx3N/hanamibot/sleep.jpg'
+      'https://github.com/SHIx3N/hanamibot/blob/main/docs/sleep.jpg'
   });
 }
 
