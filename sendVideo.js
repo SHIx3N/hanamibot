4203e0b7a4705de9df07ff0aa28b17c13a9b7f41
SHIx3N <shin.7cchi@gmail.com>
2026-03-21 21:09:05 +0900

    Update sendVideo.js

--- sendVideo.js
+++ sendVideo.js
@@ -20,7 +20,7 @@ async function waitUntil20JST() {
     const jstMin = now.getUTCMinutes();
     const jstSec = now.getUTCSeconds();
 
-    if (jstHour === 21 && jstMin === 5 && jstSec === 0) {
+    if (jstHour === 21 && jstMin === 9 && jstSec === 30) {
       console.log('20:00 JST reached');
       break;
     }
